555252b121b144255f1ed35c64ae009b2fcb2d7b
LorenzoR02 <lorenzorossi.tn@gmail.com>
2023-02-16 17:29:50 +0100

    Added auth pages

---
+++ src/pages/auth/SignOut.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SignOut() {
+  return (
+    <div>signout</div>
+  )
+}
+
+export default SignOut
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,14 +1,18 @@
 import { NextPage } from "next"
+import { useState } from "react"
 
-interface Props {}
+interface Props { }
 
 const SignIn: NextPage = (props): JSX.Element => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return <div>
     <form>
       <span>Email</span>
-      <input type='email' />
+      <input type='email' value={email} onChange={(text) => setEmail(text.target.value)} />
       <span>Password</span>
-      <input type='password' />
+      <input type='password' value={password} onChange={(text) => setPassword(text.target.value)} />
       <input type='submit' value='Sign Up' />
     </form>
   </div>
